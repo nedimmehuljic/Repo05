8634dc303808edf78c6bb19e9421cae837ca8300
nedim.mehuljic <nedim.mehuljic@hotmail.com>
2021-09-18 01:02:16 +0200

    Repo05

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+var myLink = '<a href="https://www.exapmle.com" target="_blank">Link</a>'
+    // Only change code above this line
+
+    return myLink;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+var worldBlanks;
+worldBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb;
+
+    // Only change code above this line
+
+    return worldBlanks;
+}
+
+console.log(main());
+module.exports = main;
